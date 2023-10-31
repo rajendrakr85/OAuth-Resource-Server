e4c898ecae1f5327c17dc94439595febf8040985
Rajendra Kumar <kurajendra@altimetrik.com>
2023-10-31 20:15:16 +0530

    resource server

---
+++ src/test/java/com/resource/oauthresourceserver/OAuthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource.oauthresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
